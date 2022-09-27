0284208f93659ea3b1a1180dd8940706e62c5bba
Daniel Dang <daniel.dang@zuhlke.com>
2022-09-28 05:37:20 +0700

    first commit

---
+++ src/test/java/com/example/nab/NabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.nab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
